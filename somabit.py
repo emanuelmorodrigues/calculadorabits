c51c18f3fde88a83a7825145e6a84aaf6eca975f
maneurodrigues <mrodriguesum@gmail.com>
2019-04-10 21:06:10 -0300

    codigo melhorado

--- somabit.py
+++ somabit.py
@@ -4,42 +4,40 @@ vai1 = []
 resultado = []
 
 bits = int(input("Range\n"))
+a = str(input("Valor1\n"))
+b = str(input("Valor2\n"))
 
-for i in range(bits):
-	valor = int(input("Valor1\n"))
-	num1.append(valor)
-	
-for i in range(bits):
-	valor = int(input("Valor2\n"))
-	num2.append(valor)
+num1 = [int(i) for i in a]
+num2 = [int(i) for i in b]
 
+# Define lista Vai1
+# Primeiro adicionam-se zeros para as verificações não darem "fora de range"
+# Se a soma das parcelas mais vai1 for maior ou igual a dois, então
+# significa que "vai um" na soma de bits no índice antes do atual.
 for i in range(bits):
 	vai1.append(0)
-
-# ORGANIZA O VAI1
-for i in range(-1,(-1)*bits,-1):
-	#if num1[i] + num2[i] + vai1[i] == 0:
-	#	lista.append(0)	
 	if (num1[i] + num2[i] + vai1[i] >= 2):
 		vai1[i-1] = 1
-	#elif (num1[i] + num2[i] + vai1[i] == 3):
-	#	vai1[i-1] = 1
 
-#Soma bit
+
+# Soma bit
+# Se a soma das parcelas mais vai1 for igual a zero ou igual a dois, então
+# o resultado da soma será zero. 
+# Ex: 0+0 "zero e vai 0" ou 1+1 "zero e vai 1"
+# Senão se a soma das parcelas mais vai1 for igual a um ou igual a 3, então
+# a soma será um. 
+# Ex: 0+1 (um e vai 0) ou 1+1 mais vai 1 (um e vai 1)
+
 for i in range(-1,(-1)*bits-1,-1):
 	if (num1[i] + num2[i] + vai1[i] == 0) or (num1[i] + num2[i] + vai1[i] == 2):
 		resultado.append(0)
 	elif (num1[i] + num2[i] + vai1[i] == 1) or (num1[i] + num2[i] + vai1[i] == 3):
 		resultado.append(1)
-	#elif (num1[i] + num2[i] + vai1[i] == 2):
-	#	lista.append(0)
-	#elif (num1[i] + num2[i] + vai1[i] == 3):
-	#	lista.append(1)
-#Adiciona bits quanto o resultado é uma representação maior que o range
+	# Adiciona bits quanto o resultado é uma representação maior que o range
+	# Se o i foi a ultima iteração e a soma das parcelas mais vai1 for 
+	# maior ou igual a dois, então adicione um bit 1 a mais
 	if i == (-1)*bits and vai1[i] + num1[i] + num2[i] >= 2:
 		resultado.append(1)
-	#elif i == (-1)*bits and vai1[i] + num1[i] + num2[i] == 0:
-	#	lista.append(0)
 
 resultado.reverse()
 
